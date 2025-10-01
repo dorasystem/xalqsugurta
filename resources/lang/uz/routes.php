2230c014ded1ab5d0a8206cf97e84c0a3c4fcc0f
xandjanov <zafarxandjanov2003@gmail.com>
2025-10-01 18:46:14 +0500

    Header Footer language tayyorlandi

---
+++ resources/lang/uz/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'shareholders' => 'affilangan-shaxslar',
+    'insurance' => 'sugurta',
+    'news' => 'news',
+    'useful_info' => 'foydali-malumotlar',
+    'management' => 'management',
+];
