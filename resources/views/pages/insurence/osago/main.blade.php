6ab7e8af4d05b0735744547f3723df5b09753c40
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2025-10-04 14:36:49 +0500

    api qo'shildi

--- resources/views/pages/insurence/osago/main.blade.php
+++ resources/views/pages/insurence/osago/main.blade.php
@@ -19,10 +19,10 @@
                             <div class="row gap-1">
 
                                 <x-inputs.input_form :class="'col-md-3'" :idFor="'gov_number'" :name="'gov_number'" :placeholder="'messages.gov_number_placeholder'"
-                                    :label="'messages.tech_passport_series'" />
+                                    :label="'messages.gov_number'" />
 
                                 <x-inputs.input_form :class="'col-md-2'" :idFor="'tech_passport_series'" :name="'tech_passport_series'" :placeholder="'messages.tech_passport_series_placeholder'"
-                                    :label="'messages.gov_number'" />
+                                    :label="'messages.tech_passport_series'" />
 
                                 <x-inputs.input_form :class="'col-md-3'" :idFor="'tech_passport_number'" :name="'tech_passport_number'"
                                     :placeholder="'messages.tech_passport_number_placeholder'" :label="'messages.tech_passport_number'" />
@@ -251,45 +251,47 @@
             </div>
         </div>
     </section>
-
+    <script>
+        
+    </script>
     @push('scripts')
         <script>
-            window.translations = {
-                // Messages
-                search: "{{ __('messages.search') }}",
-                searching: "{{ __('messages.searching') }}",
-                vehicle_data_loaded: "{{ __('messages.vehicle_data_loaded') }}",
-                owner_data_loaded: "{{ __('messages.owner_data_loaded') }}",
-                applicant_data_loaded: "{{ __('messages.applicant_data_loaded') }}",
-                owner_copied_as_applicant: "{{ __('messages.owner_copied_as_applicant') }}",
-                applicant_fields_cleared: "{{ __('messages.applicant_fields_cleared') }}",
-                policy_calculated: "{{ __('messages.policy_calculated') }}",
-                next_step_clicked: "{{ __('messages.next_step_clicked') }}",
-                add_driver_soon: "{{ __('messages.add_driver_soon') }}",
-                load_owner_first: "{{ __('messages.load_owner_first') }}",
+            // window.translations = {
+            //     // Messages
+            //     search: "{{ __('messages.search') }}",
+            //     searching: "{{ __('messages.searching') }}",
+            //     vehicle_data_loaded: "{{ __('messages.vehicle_data_loaded') }}",
+            //     owner_data_loaded: "{{ __('messages.owner_data_loaded') }}",
+            //     applicant_data_loaded: "{{ __('messages.applicant_data_loaded') }}",
+            //     owner_copied_as_applicant: "{{ __('messages.owner_copied_as_applicant') }}",
+            //     applicant_fields_cleared: "{{ __('messages.applicant_fields_cleared') }}",
+            //     policy_calculated: "{{ __('messages.policy_calculated') }}",
+            //     next_step_clicked: "{{ __('messages.next_step_clicked') }}",
+            //     add_driver_soon: "{{ __('messages.add_driver_soon') }}",
+            //     load_owner_first: "{{ __('messages.load_owner_first') }}",
 
-                // Errors
-                all_fields_required: "{{ __('messages.all_fields_required') }}",
-                pinfl_mismatch: "{{ __('messages.pinfl_mismatch') }}",
-                api_error: "{{ __('messages.api_error') }}",
-                request_error: "{{ __('messages.request_error') }}",
-                policy_calculation_error: "{{ __('messages.policy_calculation_error') }}",
-                select_policy_start_date: "{{ __('messages.select_policy_start_date') }}",
-                calculate_policy_first: "{{ __('messages.calculate_policy_first') }}",
+            //     // Errors
+            //     all_fields_required: "{{ __('messages.all_fields_required') }}",
+            //     pinfl_mismatch: "{{ __('messages.pinfl_mismatch') }}",
+            //     api_error: "{{ __('messages.api_error') }}",
+            //     request_error: "{{ __('messages.request_error') }}",
+            //     policy_calculation_error: "{{ __('messages.policy_calculation_error') }}",
+            //     select_policy_start_date: "{{ __('messages.select_policy_start_date') }}",
+            //     calculate_policy_first: "{{ __('messages.calculate_policy_first') }}",
 
-                // Validation
-                validation_3_letters: "{{ __('messages.validation_3_letters') }}",
-                validation_7_digits: "{{ __('messages.validation_7_digits') }}",
-                validation_year_range: "{{ __('messages.validation_year_range') }}",
+            //     // Validation
+            //     validation_3_letters: "{{ __('messages.validation_3_letters') }}",
+            //     validation_7_digits: "{{ __('messages.validation_7_digits') }}",
+            //     validation_year_range: "{{ __('messages.validation_year_range') }}",
 
-                // Period text
-                period_1_year: "{{ __('messages.1_year') }}",
-                period_6_months: "{{ __('messages.6_months') }}",
-                period_3_months: "{{ __('messages.3_months') }}",
+            //     // Period text
+            //     period_1_year: "{{ __('messages.1_year') }}",
+            //     period_6_months: "{{ __('messages.6_months') }}",
+            //     period_3_months: "{{ __('messages.3_months') }}",
 
-                // Currency
-                sum: "{{ __('messages.sum') }}"
-            };
+            //     // Currency
+            //     sum: "{{ __('messages.sum') }}"
+            // };
         </script>
         @vite(['resources/js/pages/insurence/main.js'])
     @endpush
