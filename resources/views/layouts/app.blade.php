dec16cd395b3ba33b9fdb8697ca0880402ddf7f5
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2025-10-07 11:44:06 +0500

    osago sotib olish hisob kitobi ishladi. shunchaki endi sessionga olib qo'yib ma'lumot tekshirish sahifasiga o'tkazish qoldi.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,8 @@
     <link rel="stylesheet" href="{{ asset('assets/css/form.css') }}">
 
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     
 
 
