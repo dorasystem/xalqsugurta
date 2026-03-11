22c76d3bc8fb1870241d5524be7db14738f36e40
Macbook <abbosbey2002>
2026-03-11 14:57:16 +0500

    fix error

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
@@ -22,4 +23,20 @@ class Transaction extends Model
         'paycom_time_datetime',
         'perform_time_unix',
     ];
+
+    /**
+     * Get transactions within a time range (Paycom GetStatement).
+     * from/to are Unix timestamps in milliseconds.
+     *
+     * @return Collection<int, Transaction>
+     */
+    public static function getTransactionsByTimeRange(int $from, int $to): Collection
+    {
+        return self::query()
+            ->whereNotNull('paycom_time')
+            ->whereRaw('CAST(paycom_time AS UNSIGNED) >= ?', [$from])
+            ->whereRaw('CAST(paycom_time AS UNSIGNED) <= ?', [$to])
+            ->orderBy('paycom_time')
+            ->get();
+    }
 }
